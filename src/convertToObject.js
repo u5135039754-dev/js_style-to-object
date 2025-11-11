7eaa88824a17fc9925481619c1bba9581598686f
Your Name <u513509754@gmail.com>
2025-11-11 14:06:13 -0800

    solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -1,12 +1,31 @@
 'use strict';
-
 /**
  * @param {string} sourceString
  *
  * @return {object}
  */
+
 function convertToObject(sourceString) {
-  // write your code here
+  const newSourceString = sourceString.split(';');
+  const resSourceString = [];
+
+  for (let i = 0; i < newSourceString.length; i++) {
+    const [key, value] = newSourceString[i].split(':');
+
+    if (key.trim() && value) {
+      resSourceString.push({ [key.trim()]: value.trim() });
+    }
+  }
+
+  const styles = resSourceString.reduce((accum, currentValue) => {
+    const [key, value] = Object.entries(currentValue)[0];
+
+    accum[key] = value;
+
+    return accum;
+  }, {});
+
+  return styles;
 }
 
 module.exports = convertToObject;
